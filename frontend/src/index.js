6880a21e8d19dee7abd2955558a1b23fbc33d28c
Payam Saboonchi <140518628+pym-sbnchi@users.noreply.github.com>
2024-04-04 13:11:19 +0330

    edited

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
